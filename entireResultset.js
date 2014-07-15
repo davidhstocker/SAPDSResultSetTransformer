eb819baa20751eb9ea5b1a2bd91e4ea7671bab31
David Stocker <mrdave991@googlemail.com>
2014-07-15 17:17:54 +0200

    Create entireResultset.js

---
+++ entireResultset.js
@@ -0,0 +1,96 @@
+var resultSetJSON = {
+	"selection": [-1, -1, -1], 
+	"tuples": [ [0, 0, 0], [0, 1, 0], [1, 2, 0], [1, 3, 0], [0, 0, 1], [0, 1, 1], [1, 2, 1], [1, 3, 1],  [0, 0, 2], [0, 1, 2], [1, 2, 2], [1, 3, 2], [0, 0, 3], [0, 1, 3], [1, 2, 3], [1, 3, 3] ], 
+	"data": [ 190958, 1479, 393902, 2721, 139410, 1104, 259345, 1752, 194392, 1471, 412279, 2700, 524760, 4054, 1065526, 7173 ], 
+	"dimensions": 
+		[ 
+			{ 
+				"key": "DATE", 
+				"text": "DATE", 
+				"axis": "COLUMNS", 
+				"axis_index": 0, 
+				"members": 
+					[ 
+						{ 
+							"key": "2010-01-01", 
+							"text": "01/01/2010" 
+						}, 
+						{ 
+							"key": "2012-01-01", 
+							"text": "01/01/2012" 
+						} 
+					] 
+			}, 
+			{ 
+				"key": "Measures", 
+				"text": "Measures", 
+				"axis": "COLUMNS", 
+				"axis_index": 1, 
+				"containsMeasures": true, 
+				"members": 
+					[ 
+						{ 
+							"key": "SALESREVENUE", 
+							"text": "SALESREVENUE", 
+							"formatString": "#.##0,00;'-'#.##0,00" 
+						}, 
+						{ 
+							"key": "QUANTITYSOLD", 
+							"text": "QUANTITYSOLD", 
+							"formatString": "#.##0;'-'#.##0" 
+						}, 
+						{ 
+							"key": "SALESREVENUE", 
+							"text": "SALESREVENUE", 
+							"formatString": "#.##0,00;'-'#.##0,00" 
+						}, 
+						{ 
+							"key": "QUANTITYSOLD", 
+							"text": "QUANTITYSOLD",
+							"formatString": "#.##0;'-'#.##0" 
+						} 
+					] 
+			}, 
+			{
+				"key": "CITY", 
+				"text": "CITY", 
+				"axis": "ROWS", 
+				"axis_index": 0, 
+				"members": 
+					[ 
+						{ 
+							"key": "Berlin", 
+							"text": "Berlin" 
+						}, 
+						{ 
+							"key": "Rio de Janeiro", 
+							"text": "Rio de Janeiro" 
+						}, 
+						{ 
+							"key": "Tokyo", 
+							"text": "Tokyo" 
+						}, 
+						{ 
+							"key": "Result", 
+							"text": "Overall Result", 
+							"type": "RESULT" 
+						} 
+					] 
+			} 
+		], 
+	"axis_columns": 
+		[ 
+			[0, 0, -1], 
+			[0, 1, -1], 
+			[1, 2, -1], 
+			[1, 3, -1] 
+		], 
+	"axis_rows": 
+		[ 
+			[-1, -1, 0], 
+			[-1, -1, 1], 
+			[-1, -1, 2], 
+			[-1, -1, 3] 
+		], 
+	"locale": "en_US"
+};
